c70be0697e4a8b90e8247884bb202ac047fb27c8
Maximilian <maxigertz@gmail.com>
2019-06-30 18:50:11 +0200

    Ratesystem v1.0

--- cywwtaip/src/manage/RateSystem.java
+++ cywwtaip/src/manage/RateSystem.java
@@ -1,28 +1,64 @@
 package manage;
 
 import bots.Bot;
+import bots.BotType;
 
 public class RateSystem {
 
-    final float NORMAL_GET_ENERGY = 1.5f;
-    final float MOBILE_GET_ENERGY = 1.0f;
-    final float WIDE_GET_ENERGY = 1.0f;
+    final float DISTANCE_RELATION = 10;
+    final float VIEW_DISTANCE_RANGE = 5;
 
-    final float NORMAL_REPAINT = 1.0f;
-    final float MOBILE_REPAINT = 1.0f;
-    final float WIDE_REPAINT = 1.0f;
+    private final float NORMAL_GET_ENERGY = 1.5f;
+    private final float MOBILE_GET_ENERGY = 1.0f;
+    private final float WIDE_GET_ENERGY = 1.0f;
 
-    final float NORMAL_KEEP_DISTANCE = 1.0f;
-    final float MOBILE_KEEP_DISTANCE = 1.0f;
-    final float WIDE_KEEP_DISTANCE = 1.0f;
+    private final float NORMAL_REPAINT = 1.0f;
+    private final float MOBILE_REPAINT = 1.0f;
+    private final float WIDE_REPAINT = 1.0f;
 
-    public float[] generateRating(Bot bot){
-        float goToEnergySupply;
-        float attackOtherPlayer;
-        float keepDistance;
+    private final float NORMAL_KEEP_DISTANCE = 1.0f;
+    private final float MOBILE_KEEP_DISTANCE = 1.0f;
+    private final float WIDE_KEEP_DISTANCE = 1.0f;
 
-        bot.getDistanceToSupply();
+    public float[] generateRating(Bot bot, int myPlayernumber){
+        if (bot.botType== BotType.NORMAL)
+            return generate(bot, myPlayernumber, NORMAL_GET_ENERGY, NORMAL_KEEP_DISTANCE, NORMAL_REPAINT);
+
+        if (bot.botType== BotType.MOBILE)
+            return generate(bot, myPlayernumber, MOBILE_GET_ENERGY, MOBILE_KEEP_DISTANCE, MOBILE_REPAINT);
+
+        if (bot.botType== BotType.WIDE)
+            return generate(bot, myPlayernumber, WIDE_GET_ENERGY, WIDE_KEEP_DISTANCE, WIDE_REPAINT);
 
         return null;
+    }
+
+    private float[] getDistancesToEnemies(Bot bot, int playernumber){
+        float dis1 = 0;
+        float dis2 = 0;
+        boolean findSecond = false;
+
+        for (int i = 0; i < 3; i++){
+            if (i != playernumber){
+                if(findSecond)
+                    dis2= bot.getDistanceToPlayerNode(i);
+                dis1 = bot.getDistanceToPlayerNode(i);
+                findSecond = true;
+            }
+
+        }
+        float[] result = {dis1, dis2};
+        return result;
+    }
+
+    private float[] generate(Bot bot, int myplayernumber, float multEnergy, float multPassive, float multRepaint){
+        float distanceToPowerSupply = bot.getDistanceToSupply();
+        float[] distanceToEnemyNodes = getDistancesToEnemies(bot, myplayernumber);
+        float[] rating = {
+                (1/distanceToPowerSupply) * multEnergy,
+                (distanceToEnemyNodes[0]+distanceToEnemyNodes[1]) /2 * multPassive,
+                (1/(distanceToEnemyNodes[0]+distanceToEnemyNodes[1]) /2 * multRepaint)
+        };
+        return rating;
     }
 }
